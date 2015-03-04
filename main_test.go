e697dea8d445619763a09f067e69bebc76f8d5e2
Luke Mallon <luke.mallon@axonista.com>
2015-03-04 13:19:56 +0000

    Adding test file for main.go

---
+++ main_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestMain(t *testing.T) {
+	t.Error("main is failing because there is no test.")
+}
+
+func TestVarnishStat(t *testing.T) {
+	t.Error("varnishStat is failing because there is no test.")
+}
